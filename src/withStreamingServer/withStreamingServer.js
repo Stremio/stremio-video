bc352938d2b0651ad95b71a226204fd31ff5a637
nklhrstv <me@nikolahristov.com>
2021-08-31 17:39:50 +0300

    log a warning when probe media failed

--- src/withStreamingServer/withStreamingServer.js
+++ src/withStreamingServer/withStreamingServer.js
@@ -97,7 +97,10 @@ function withStreamingServer(Video) {
                         convertStream(commandArgs.streamingServerURL, commandArgs.stream, commandArgs.seriesInfo)
                             .then(function(mediaURL) {
                                 return (commandArgs.forceTranscoding ? Promise.resolve(false) : Video.canPlayStream({ url: mediaURL }))
-                                    .catch(function() { return false; })
+                                    .catch(function(error) {
+                                        console.warn('Media probe error', error);
+                                        return false;
+                                    })
                                     .then(function(canPlay) {
                                         if (canPlay) {
                                             return {
