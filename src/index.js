b6b52da62ea087c74b7a8ad858cd97669e002749
nklhrstv <me@nikolahristov.com>
2021-09-02 16:32:10 +0300

    export only StremioVideo by default

--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
 var StremioVideo = require('./StremioVideo');
-var IFrameVideo = require('./IFrameVideo');
-var HTMLVideo = require('./HTMLVideo');
-var YouTubeVideo = require('./YouTubeVideo');
-var ChromecastSenderVideo = require('./ChromecastSenderVideo');
-var withHTMLSubtitles = require('./withHTMLSubtitles');
-var withStreamingServer = require('./withStreamingServer');
 
-module.exports = {
-    StremioVideo: StremioVideo,
-    IFrameVideo: IFrameVideo,
-    HTMLVideo: HTMLVideo,
-    YouTubeVideo: YouTubeVideo,
-    ChromecastSenderVideo: ChromecastSenderVideo,
-    withHTMLSubtitles: withHTMLSubtitles,
-    withStreamingServer: withStreamingServer
-};
+module.exports = StremioVideo;
