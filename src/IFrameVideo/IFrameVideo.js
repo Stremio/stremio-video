109bee3e751ed71561ee0525b251bf9baa87cd8c
Alexandru Branza <branza.alex@gmail.com>
2022-05-07 09:03:13 +0300

    Complete iFrame Player Support

--- src/IFrameVideo/IFrameVideo.js
+++ src/IFrameVideo/IFrameVideo.js
@@ -3,6 +3,10 @@ var cloneDeep = require('lodash.clonedeep');
 var deepFreeze = require('deep-freeze');
 var ERROR = require('../error');
 
+var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
+var eventer = window[eventMethod];
+var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
+
 function IFrameVideo(options) {
     options = options || {};
 
@@ -17,13 +21,45 @@ function IFrameVideo(options) {
     iframeElement.style.border = 0;
     iframeElement.style.backgroundColor = 'black';
     iframeElement.allowFullscreen = false;
+    iframeElement.allow = 'autoplay';
     containerElement.appendChild(iframeElement);
+
+    // Listen to message from child window
+    eventer(messageEvent,function(e) {
+        source = e.source;
+        var key = e.message ? "message" : "data";
+        var data = e[key];
+        if ((data || {}).propName) {
+            iframeProps[data.propName] = data.propValue;
+            onPropChanged(data.propName, data.propValue);
+        }
+    }, false);
 
     var events = new EventEmitter();
     var destroyed = false;
     var stream = null;
+    var source = false;
+
+    var iframeProps = {
+        paused: false,
+        time: 0,
+        duration: 0,
+        buffering: 0,
+        buffered: false,
+        volume: 100,
+        muted: false,
+        playbackSpeed: false
+    }
     var observedProps = {
-        stream: false
+        stream: false,
+        paused: false,
+        time: false,
+        duration: false,
+        buffering: false,
+        buffered: false,
+        volume: false,
+        muted: false,
+        playbackSpeed: false
     };
 
     function getProp(propName) {
@@ -31,6 +67,31 @@ function IFrameVideo(options) {
             case 'stream': {
                 return stream;
             }
+            case 'paused': {
+                return iframeProps.paused;
+            }
+            case 'time': {
+                return iframeProps.time;
+            }
+            case 'duration': {
+                return iframeProps.duration;
+            }
+            case 'buffered': {
+                return iframeProps.buffered;
+            }
+            case 'buffering': {
+                return iframeProps.buffering;
+            }
+            case 'volume': {
+                return iframeProps.volume;
+            }
+            case 'muted': {
+                return iframeProps.muted;
+            }
+            case 'playbackSpeed': {
+                return iframeProps.playbackSpeed;
+            }
+
             default: {
                 return null;
             }
@@ -53,6 +114,11 @@ function IFrameVideo(options) {
             observedProps[propName] = true;
         }
     }
+    function setProp(propName, propValue) {
+        if (source) {
+            source.postMessage({ propName, propValue }, '*');
+        }
+    }
     function command(commandName, commandArgs) {
         switch (commandName) {
             case 'load': {
@@ -71,6 +137,7 @@ function IFrameVideo(options) {
             }
             case 'unload': {
                 stream = null;
+                source = null;
                 iframeElement.removeAttribute('src');
                 onPropChanged('stream');
                 break;
@@ -104,6 +171,10 @@ function IFrameVideo(options) {
                     observeProp(action.propName);
                     return;
                 }
+                case 'setProp': {
+                    setProp(action.propName, action.propValue);
+                    return;
+                }
                 case 'command': {
                     command(action.commandName, action.commandArgs);
                     return;
@@ -122,7 +193,7 @@ IFrameVideo.canPlayStream = function(stream) {
 IFrameVideo.manifest = {
     name: 'IFrameVideo',
     external: false,
-    props: ['stream'],
+    props: ['stream', 'paused', 'time', 'duration', 'buffering', 'buffered', 'volume', 'muted', 'playbackSpeed'],
     commands: ['load', 'unload', 'destroy'],
     events: ['propValue', 'propChanged', 'error']
 };
