2f4d5dd23162a6f49b06ae5d1ef89abe5aa72809
nklhrstv <me@nikolahristov.com>
2022-11-24 17:20:06 +0200

    media capabilities module added

---
+++ src/mediaCapabilities.js
@@ -0,0 +1,81 @@
+var VIDEO_CODECS_CONFIG = [
+    {
+        codec: 'h264',
+        mime: 'video/mp4; codecs="avc1.42E01E"',
+    },
+    {
+        codec: 'h265',
+        mime: 'video/mp4; codecs="hev1.1.6.L150.B0"',
+        aliases: ['hevc']
+    },
+    {
+        codec: 'vp8',
+        mime: 'video/mp4; codecs="vp8"'
+    },
+    {
+        codec: 'vp9',
+        mime: 'video/mp4; codecs="vp9"'
+    }
+];
+
+var AUDIO_CODECS_CONFIG = [
+    {
+        codec: 'aac',
+        mime: 'audio/mp4; codecs="mp4a.40.2"'
+    },
+    {
+        codec: 'mp3',
+        mime: 'audio/mp4; codecs="mp3"'
+    },
+    {
+        codec: 'ac3',
+        mime: 'audio/mp4; codecs="ac-3"'
+    },
+    {
+        codec: 'eac3',
+        mime: 'audio/mp4; codecs="ec-3"'
+    },
+    {
+        codec: 'vorbis',
+        mime: 'audio/mp4; codecs="vorbis"'
+    },
+    {
+        codec: 'opus',
+        mime: 'audio/mp4; codecs="opus"'
+    }
+];
+
+function canPlay(config, options) {
+    return options.mediaElement.canPlayType(config.mime) ?
+        [config.codec].concat(config.aliases || [])
+        :
+        [];
+}
+
+function getMediaCapabilities() {
+    var mediaElement = document.createElement('video');
+    var videoCodecs = VIDEO_CODECS_CONFIG
+        .map(function(config) {
+            return canPlay(config, {
+                mediaElement: mediaElement
+            });
+        })
+        .reduce(function(result, value) {
+            return result.concat(value);
+        }, []);
+    var audioCodecs = AUDIO_CODECS_CONFIG
+        .map(function(config) {
+            return canPlay(config, {
+                mediaElement: mediaElement
+            });
+        })
+        .reduce(function(result, value) {
+            return result.concat(value);
+        }, []);
+    return {
+        videoCodecs: videoCodecs,
+        audioCodecs: audioCodecs
+    };
+}
+
+module.exports = getMediaCapabilities;
