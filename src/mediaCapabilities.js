a68655c855cfe13429b554f8b59cb2f207ebeed6
unclekingpin <unclekingpin@proton.me>
2023-11-05 07:34:48 -0800

    revert format specific codec checks

--- src/mediaCapabilities.js
+++ src/mediaCapabilities.js
@@ -1,94 +1,51 @@
-var MP4_CONFIG = {
-    VIDEO_CODECS: [
-        {
-            codec: 'h264',
-            force: window.chrome || window.cast,
-            mime: 'video/mp4; codecs="avc1.42E01E"',
-        },
-        {
-            codec: 'h265',
-            force: window.chrome || window.cast,
-            mime: 'video/mp4; codecs="hev1.1.6.L150.B0"',
-            aliases: ['hevc'],
-        },
-        {
-            codec: 'vp8',
-            mime: 'video/mp4; codecs="vp8"',
-        },
-        {
-            codec: 'vp9',
-            mime: 'video/mp4; codecs="vp9"',
-        },
-    ],
-    AUDIO_CODEC: [
-        {
-            codec: 'aac',
-            force: window.chrome || window.cast,
-            mime: 'audio/mp4; codecs="mp4a.40.2"',
-        },
-        {
-            codec: 'mp3',
-            force: window.chrome || window.cast,
-            mime: 'audio/mp4; codecs="mp3"',
-        },
-        {
-            codec: 'ac3',
-            mime: 'audio/mp4; codecs="ac-3"',
-        },
-        {
-            codec: 'eac3',
-            mime: 'audio/mp4; codecs="ec-3"',
-        },
-        {
-            codec: 'vorbis',
-            mime: 'audio/mp4; codecs="vorbis"',
-        },
-        {
-            codec: 'opus',
-            mime: 'audio/mp4; codecs="opus"',
-        },
-    ],
-};
+var VIDEO_CODEC_CONFIGS = [
+    {
+        codec: 'h264',
+        force: window.chrome || window.cast,
+        mime: 'video/mp4; codecs="avc1.42E01E"',
+    },
+    {
+        codec: 'h265',
+        force: window.chrome || window.cast,
+        mime: 'video/mp4; codecs="hev1.1.6.L150.B0"',
+        aliases: ['hevc']
+    },
+    {
+        codec: 'vp8',
+        mime: 'video/mp4; codecs="vp8"'
+    },
+    {
+        codec: 'vp9',
+        mime: 'video/mp4; codecs="vp9"'
+    }
+];
 
-var MATROSKA_CONFIG = {
-    VIDEO_CODECS: [
-        {
-            codec: 'h264',
-            force: window.chrome || window.cast,
-        },
-        {
-            codec: 'h265',
-            force: window.chrome || window.cast,
-            aliases: ['hevc'],
-        },
-        {
-            codec: 'vp8',
-            mime: 'video/webm; codecs="vp8"',
-        },
-        {
-            codec: 'vp9',
-            mime: 'video/webm; codecs="vp9"',
-        },
-    ],
-    AUDIO_CODEC: [
-        {
-            codec: 'aac',
-            force: window.chrome || window.cast,
-        },
-        {
-            codec: 'mp3',
-            force: window.chrome || window.cast,
-        },
-        {
-            codec: 'vorbis',
-            mime: 'audio/webm; codecs="vorbis"',
-        },
-        {
-            codec: 'opus',
-            mime: 'audio/webm; codecs="opus"',
-        },
-    ],
-};
+var AUDIO_CODEC_CONFIGS = [
+    {
+        codec: 'aac',
+        mime: 'audio/mp4; codecs="mp4a.40.2"'
+    },
+    {
+        codec: 'mp3',
+        mime: 'audio/mp4; codecs="mp3"'
+    },
+    {
+        codec: 'ac3',
+        mime: 'audio/mp4; codecs="ac-3"'
+    },
+    {
+        codec: 'eac3',
+        mime: 'audio/mp4; codecs="ec-3"'
+    },
+    {
+        codec: 'vorbis',
+        mime: 'audio/mp4; codecs="vorbis"'
+    },
+    {
+        codec: 'opus',
+        mime: 'audio/mp4; codecs="opus"'
+    }
+];
 
 function canPlay(config, options) {
     return config.force || options.mediaElement.canPlayType(config.mime)
@@ -111,34 +68,27 @@ function getMaxAudioChannels() {
 
 function getMediaCapabilities() {
     var mediaElement = document.createElement('video');
+    var formats = ['mp4', 'matroska,webm'];
+    var videoCodecs = VIDEO_CODEC_CONFIGS
+        .map(function (config) {
+            return canPlay(config, { mediaElement: mediaElement });
+        })
+        .reduce(function (result, value) {
+            return result.concat(value);
+        }, []);
+    var audioCodecs = AUDIO_CODEC_CONFIGS
+        .map(function (config) {
+            return canPlay(config, { mediaElement: mediaElement });
+        })
+        .reduce(function (result, value) {
+            return result.concat(value);
+        }, []);
     var maxAudioChannels = getMaxAudioChannels();
     return {
-        mp4: {
-            videoCodecs: MP4_CONFIG.VIDEO_CODECS.map(function(config) {
-                return canPlay(config, { mediaElement: mediaElement });
-            }).reduce(function(result, value) {
-                return result.concat(value);
-            }, []),
-            audioCodecs: MP4_CONFIG.AUDIO_CODEC.map(function(config) {
-                return canPlay(config, { mediaElement: mediaElement });
-            }).reduce(function(result, value) {
-                return result.concat(value);
-            }, []),
-            maxAudioChannels: maxAudioChannels
-        },
-        'matroska,webm': {
-            videoCodecs: MATROSKA_CONFIG.VIDEO_CODECS.map(function(config) {
-                return canPlay(config, { mediaElement: mediaElement });
-            }).reduce(function(result, value) {
-                return result.concat(value);
-            }, []),
-            audioCodecs: MATROSKA_CONFIG.AUDIO_CODEC.map(function(config) {
-                return canPlay(config, { mediaElement: mediaElement });
-            }).reduce(function(result, value) {
-                return result.concat(value);
-            }, []),
-            maxAudioChannels: maxAudioChannels
-        },
+        formats: formats,
+        videoCodecs: videoCodecs,
+        audioCodecs: audioCodecs,
+        maxAudioChannels: maxAudioChannels
     };
 }
 
