69e410262709510770c5d9b9d6f3eefef0075d48
Alexandru Branza <branza.alex@gmail.com>
2024-02-25 15:03:36 +0200

    missing require added

--- src/withStreamingServer/destroyHLSConverter.js
+++ src/withStreamingServer/destroyHLSConverter.js
@@ -1,3 +1,5 @@
+var url = require('url');
+
 function destroyHLSConverter(streamingServerURL, id) {
     return fetch(url.resolve(streamingServerURL, '/hlsv2/' + encodeURIComponent(id) + '/destroy'));
 }
