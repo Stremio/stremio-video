2fed5bb2f0d26ea1cfd303114246dc452341b7ad
Alexandru Branza <branza.alex@gmail.com>
2024-03-12 23:06:13 +0200

    Support stream.fileMustInclude 2

--- src/withStreamingServer/createTorrent.js
+++ src/withStreamingServer/createTorrent.js
@@ -1,23 +1,30 @@
 var url = require('url');
 
-function buildTorrent(streamingServerURL, infoHash, fileIdx, sources) {
+function buildTorrent(streamingServerURL, infoHash, fileIdx, fileMustInclude, sources) {
     var query = Array.isArray(sources) && sources.length > 0 ?
         '?' + new URLSearchParams(sources.map(function(source) {
             return ['tr', source];
         }))
         :
         '';
+    query = query + (Array.isArray(fileMustInclude) && fileMustInclude.length > 0 ?
+        (query ? '&' : '?') + new URLSearchParams(fileMustInclude.map(function(mustInclude) {
+            return ['f', mustInclude];
+        }))
+        :
+        '');
     return {
-        url: url.resolve(streamingServerURL, '/' + encodeURIComponent(infoHash) + '/' + encodeURIComponent(fileIdx)) + query,
+        url: url.resolve(streamingServerURL, '/' + encodeURIComponent(infoHash) + '/' + encodeURIComponent(fileIdx || -1)) + query,
         infoHash: infoHash,
         fileIdx: fileIdx,
+        fileMustInclude: fileMustInclude,
         sources: sources
     };
 }
 
 function createTorrent(streamingServerURL, infoHash, fileIdx, fileMustInclude, sources, seriesInfo) {
-    if ((!Array.isArray(sources) || sources.length === 0) && (fileIdx !== null && isFinite(fileIdx))) {
-        return Promise.resolve(buildTorrent(streamingServerURL, infoHash, fileIdx, sources));
+    if ((!Array.isArray(sources) || sources.length === 0) && ((fileIdx !== null && isFinite(fileIdx)) || (fileMustInclude || []).length)) {
+        return Promise.resolve(buildTorrent(streamingServerURL, infoHash, fileIdx, fileMustInclude, sources));
     }
 
     var body = {
@@ -67,7 +74,7 @@ function createTorrent(streamingServerURL, infoHash, fileIdx, fileMustInclude, s
 
         throw new Error(resp.status + ' (' + resp.statusText + ')');
     }).then(function(resp) {
-        return buildTorrent(streamingServerURL, infoHash, body.guessFileIdx ? resp.guessedFileIdx : fileIdx, body.peerSearch ? body.peerSearch.sources : []);
+        return buildTorrent(streamingServerURL, infoHash, body.guessFileIdx ? resp.guessedFileIdx : fileIdx, fileMustInclude, body.peerSearch ? body.peerSearch.sources : []);
     });
 }
 
