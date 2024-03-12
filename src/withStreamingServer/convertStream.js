459b6fdf06f6786926835b7ff276dd0013123933
Alexandru Branza <branza.alex@gmail.com>
2024-03-12 23:21:18 +0200

    Support stream.fileMustInclude 3

--- src/withStreamingServer/convertStream.js
+++ src/withStreamingServer/convertStream.js
@@ -24,7 +24,7 @@ function convertStream(streamingServerURL, stream, seriesInfo) {
                     [];
                 createTorrent(streamingServerURL, parsedMagnetURI.infoHash, null, null, sources, seriesInfo)
                     .then(function(torrent) {
-                        resolve({ url: torrent.url, infoHash: torrent.infoHash, fileIdx: torrent.fileIdx });
+                        resolve({ url: torrent.url, infoHash: torrent.infoHash, fileIdx: torrent.fileIdx, fileMustInclude: torrent.fileMustInclude });
                     })
                     .catch(function(error) {
                         reject(error);
@@ -39,7 +39,7 @@ function convertStream(streamingServerURL, stream, seriesInfo) {
         if (typeof stream.infoHash === 'string') {
             createTorrent(streamingServerURL, stream.infoHash, stream.fileIdx, stream.fileMustInclude, stream.announce, seriesInfo)
                 .then(function(torrent) {
-                    resolve({ url: torrent.url, infoHash: torrent.infoHash, fileIdx: torrent.fileIdx });
+                    resolve({ url: torrent.url, infoHash: torrent.infoHash, fileIdx: torrent.fileIdx, fileMustInclude: torrent.fileMustInclude });
                 })
                 .catch(function(error) {
                     reject(error);
