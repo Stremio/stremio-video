326ea8ef50e75f03fb4cbd6ec4986a3853534cf4
Alexandru Branza <branza.alex@gmail.com>
2024-06-10 13:39:18 +0300

    Fix Tracks Data When Array Elements are `null`

--- src/tracksData.js
+++ src/tracksData.js
@@ -2,8 +2,8 @@ module.exports = function(url, cb) {
     fetch('http://127.0.0.1:11470/tracks/'+encodeURIComponent(url)).then(function(resp) {
         return resp.json();
     }).then(function(tracks) {
-        var audioTracks = tracks.filter(function(el) { return el.type === 'audio'; });
-        var subsTracks = tracks.filter(function(el) { return el.type === 'text'; });
+        var audioTracks = tracks.filter(function(el) { return (el || {}).type === 'audio'; });
+        var subsTracks = tracks.filter(function(el) { return (el || {}).type === 'text'; });
         cb({ audio: audioTracks, subs: subsTracks });
     }).catch(function(err) {
         // eslint-disable-next-line no-console
