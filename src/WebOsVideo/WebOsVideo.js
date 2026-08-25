362380d286b0e9a306c909b17352d8ca746e4056
Alexandru Branza <branza.alex@gmail.com>
2026-08-25 17:08:43 +0300

    Revert "Fix hls.js for WebOS"
    This reverts commit f9efe15513214536676987c0598858e47c64afa1.

--- src/WebOsVideo/WebOsVideo.js
+++ src/WebOsVideo/WebOsVideo.js
@@ -855,46 +855,47 @@ function WebOsVideo(options) {
                         onPropChanged('selectedAudioTrackId');
                         events.emit('audioTrackLoaded', selectedAudioTrack);
                     }
-                } else {
-                    if ((propValue || '').indexOf('EMBEDDED_') === 0) {
-                        currentAudioTrack = propValue;
-                        var trackIndex = parseInt(propValue.replace('EMBEDDED_', ''));
-                        if (videoElement.mediaId) {
-                            luna({
-                                method: 'selectTrack',
-                                parameters: {
-                                    'type': 'audio',
-                                    'mediaId': videoElement.mediaId,
-                                    'index': trackIndex
-                                }
-                            }, function() {
-                                var selectedAudioTrack = getProp('audioTracks')
-                                    .find(function(track) {
-                                        return track.id === propValue;
-                                    });
+                    return;
+                }
 
-                                audioTracks = audioTracks.map(function(track) {
-                                    track.mode = track.id === currentAudioTrack ? 'showing' : 'disabled';
-                                    return track;
+                if ((propValue || '').indexOf('EMBEDDED_') === 0) {
+                    currentAudioTrack = propValue;
+                    var trackIndex = parseInt(propValue.replace('EMBEDDED_', ''));
+                    if (videoElement.mediaId) {
+                        luna({
+                            method: 'selectTrack',
+                            parameters: {
+                                'type': 'audio',
+                                'mediaId': videoElement.mediaId,
+                                'index': trackIndex
+                            }
+                        }, function() {
+                            var selectedAudioTrack = getProp('audioTracks')
+                                .find(function(track) {
+                                    return track.id === propValue;
                                 });
 
-                                if (selectedAudioTrack) {
-                                    events.emit('audioTrackLoaded', selectedAudioTrack);
-                                    onPropChanged('selectedAudioTrackId');
-                                }
+                            audioTracks = audioTracks.map(function(track) {
+                                track.mode = track.id === currentAudioTrack ? 'showing' : 'disabled';
+                                return track;
                             });
-                        }
-                        if (videoElement && videoElement.audioTracks) {
-                            for (var i = 0; i < videoElement.audioTracks.length; i++) {
-                                videoElement.audioTracks[i].enabled = false;
-                            }
 
-                            if(videoElement.audioTracks[trackIndex]) {
-                                videoElement.audioTracks[trackIndex].enabled = true;
+                            if (selectedAudioTrack) {
+                                events.emit('audioTrackLoaded', selectedAudioTrack);
+                                onPropChanged('selectedAudioTrackId');
                             }
-                        }
-
+                        });
                     }
+                    if (videoElement && videoElement.audioTracks) {
+                        for (var i = 0; i < videoElement.audioTracks.length; i++) {
+                            videoElement.audioTracks[i].enabled = false;
+                        }
+
+                        if(videoElement.audioTracks[trackIndex]) {
+                            videoElement.audioTracks[trackIndex].enabled = true;
+                        }
+                    }
+
                 }
 
                 break;
@@ -972,56 +973,56 @@ function WebOsVideo(options) {
                         });
                         hls.loadSource(stream.url);
                         hls.attachMedia(videoElement);
-                    } else {
-
-                        var count = 0;
-
-                        var initMediaId = function (cb) {
-                            function retrieveMediaId() {
-                                if (videoElement.mediaId) {
-                                    clearInterval(timer);
-                                    retrieveExtendedTracks();
-                                    retrieveDeviceInfo();
-                                    cb();
-                                    return;
-                                }
-                                count++;
-                                if (count > 4) {
-                                    // console.log('failed to get media id');
-                                    clearInterval(timer);
-                                    retrieveExtendedTracks();
-                                    retrieveDeviceInfo();
-                                    cb();
-                                }
-                            }
-                            var timer = setInterval(retrieveMediaId, 300);
-                        };
-
-                        var startVideo = function () {
-                            // console.log('startVideo');
-                            // not needed?
-                            // videoElement.src = stream.url;
-
-                            try {
-                                videoElement.load();
-                            } catch(_e) {
-                                // console.log('can\'t load video');
-                                // console.error(e);
-                            }
-
-                            try {
-                                // console.log('try play');
-                                videoElement.play();
-                            } catch(_e) {
-                                // console.log('can\'t start video');
-                                // console.error(e);
-                            }
-                        };
-
-                        videoElement.src = stream.url;
-
-                        initMediaId(startVideo);
+                        return;
                     }
+
+                    var count = 0;
+
+                    var initMediaId = function (cb) {
+                        function retrieveMediaId() {
+                            if (videoElement.mediaId) {
+                                clearInterval(timer);
+                                retrieveExtendedTracks();
+                                retrieveDeviceInfo();
+                                cb();
+                                return;
+                            }
+                            count++;
+                            if (count > 4) {
+                                // console.log('failed to get media id');
+                                clearInterval(timer);
+                                retrieveExtendedTracks();
+                                retrieveDeviceInfo();
+                                cb();
+                            }
+                        }
+                        var timer = setInterval(retrieveMediaId, 300);
+                    };
+
+                    var startVideo = function () {
+                        // console.log('startVideo');
+                        // not needed?
+                        // videoElement.src = stream.url;
+
+                        try {
+                            videoElement.load();
+                        } catch(_e) {
+                            // console.log('can\'t load video');
+                            // console.error(e);
+                        }
+
+                        try {
+                            // console.log('try play');
+                            videoElement.play();
+                        } catch(_e) {
+                            // console.log('can\'t start video');
+                            // console.error(e);
+                        }
+                    };
+
+                    videoElement.src = stream.url;
+
+                    initMediaId(startVideo);
                 } else {
                     onError(Object.assign({}, ERROR.UNSUPPORTED_STREAM, {
                         critical: true,
