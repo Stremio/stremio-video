38b63353ce13f7540662341ec4b45caa49a6b651
Timothy Z. <timothy@stremio.com>
2026-09-05 13:32:05 +0300

    Preserve raw ASS sources through the streaming server

--- src/withStreamingServer/convertStream.js
+++ src/withStreamingServer/convertStream.js
@@ -1,19 +1,6 @@
-var url = require('url');
 var magnet = require('magnet-uri');
 var createTorrent = require('./createTorrent');
-
-function buildProxyUrl(streamingServerURL, streamURL, requestHeaders, responseHeaders) {
-    var parsedStreamURL = new URL(streamURL);
-    var proxyOptions = new URLSearchParams();
-    proxyOptions.set('d', parsedStreamURL.origin);
-    Object.entries(requestHeaders).forEach(function(entry) {
-        proxyOptions.append('h', entry[0] + ':' + entry[1]);
-    });
-    Object.entries(responseHeaders).forEach(function(entry) {
-        proxyOptions.append('r', entry[0] + ':' + entry[1]);
-    });
-    return url.resolve(streamingServerURL, '/proxy/' + proxyOptions.toString() + parsedStreamURL.pathname) + parsedStreamURL.search;
-}
+var buildProxyUrl = require('./buildProxyUrl');
 
 function convertStream(streamingServerURL, stream, seriesInfo, streamingServerSettings) {
     return new Promise(function(resolve, reject) {
